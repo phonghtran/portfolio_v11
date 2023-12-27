6e3f16cfd524aa933c2e1f6434bd641ae6c33d07
phonghtran <phong@phonghtran.com>
2023-12-27 16:02:20 -0500

    testing with todo mdn code component deploy to firebase static

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	import Slider from './../components/Slider.svelte';
+
+	let name = 'test';
+</script>
+
+<h1>Welcome to SvelteKit.</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Slider {name} />
