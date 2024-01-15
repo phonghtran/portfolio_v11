76de11c8e60a023af2bef706885b49504a1939d7
phonghtran <phong@phonghtran.com>
2024-01-14 22:48:48 -0500

    stable audio player

--- src/routes/audio/+page.svelte
+++ src/routes/audio/+page.svelte
@@ -3,7 +3,7 @@
 
 	import { onMount } from 'svelte';
 
-	let isPlaying = false;
+	let isPlaying = -1;
 
 	let audioFiles = [
 		{
@@ -11,8 +11,7 @@
 			label: 'History and background ',
 			ogg: '../audio/001_background-history.ogg',
 			mp3: '../audio/001_background-history.mp3',
-			subtitles: '../audio/001_background-history.vtt',
-			isActive: false
+			subtitles: '../audio/001_background-history.vtt'
 		},
 		{
 			id: 1,
@@ -37,14 +36,14 @@
 		},
 		{
 			id: 4,
-			label: 'Tell me about a recent project you’re proud of? ',
+			label: 'Tell me about a recent project ',
 			ogg: '../audio/005_recent-project-ai.ogg',
 			mp3: '../audio/005_recent-project-ai.mp3',
 			subtitles: '../audio/005_recent-project-ai.vtt'
 		},
 		{
 			id: 5,
-			label: 'Tell me about a time where you had a hurdle',
+			label: 'Tell me about a recent hurdle',
 			ogg: '../audio/006_hurdle-ai.ogg',
 			mp3: '../audio/006_hurdle-ai.mp3',
 			subtitles: '../audio/006_hurdle-ai.vtt'
@@ -66,170 +65,118 @@
 	];
 
 	/** @param {MouseEvent} event */
-	function pauseAudio(event) {
-		console.log('i see');
-		const video = document.getElementById('video');
-		if (video.paused || video.ended) {
-			video.play();
-			isPlaying = true;
-		} else {
+	function pauseAudio(index) {
+		const video = document.getElementById('video' + index);
+
+		console.log('clicked', index, isPlaying);
+
+		if (isPlaying === index) {
+			// is currently playing
+			console.log('pausing the current track');
+
 			video.pause();
-			isPlaying = false;
-		}
-	}
-	function resetVideo() {
-		let video;
-		let metadataTrack;
+			isPlaying = -1;
+		} else if (isPlaying === -1) {
+			// nothing is playing
+			console.log('resuming current track');
+			video.play();
+			isPlaying = index;
+		} else {
+			// different track is playing
+			console.log('pausing current track and starting new track');
 
-		var elems = document.getElementsByTagName('video');
-		for (let i = 0; i < elems.length; i++) {
-			const thisVideo = elems[i];
-			thisVideo.pause();
-			thisVideo.currentTime = 0;
+			const currentVideo = document.getElementById('video' + isPlaying);
 
-			if (thisVideo.hasChildNodes()) {
-				while (thisVideo.firstChild) {
-					thisVideo.removeChild(thisVideo.firstChild);
-				}
-			}
-
-			thisVideo.remove();
+			currentVideo.pause();
+			video.play();
+			isPlaying = index;
 		}
 
-		for (let i = 0; i < audioFiles.length; i++) {
-			audioFiles[i]['isActive'] = false;
-		}
-
-		const progress = document.getElementById('progress');
-		progress.value = 0;
+		console.log('wrapping', index, isPlaying);
 	}
 
-	function loadAudioFile(id) {
-		console.log(id);
-		resetVideo();
+	function setProgressDuration(event, index, video) {
+		const progress = document.getElementById('progress' + index);
 
-		const containerScript = document.getElementById('containerScript' + id);
-		const progress = document.getElementById('progress');
-		const progressBar = document.getElementById('progress-bar');
+		progress.setAttribute('max', video.duration);
+	}
 
-		const s_ogg = document.createElement('source');
-		s_ogg.src = audioFiles[id]['ogg'];
-		s_ogg.type = 'video/ogg';
+	function updateProgress(event, index, video) {
+		const progress = document.getElementById('progress' + index);
 
-		const s_mp3 = document.createElement('source');
-		s_mp3.src = audioFiles[id]['mp3'];
-		s_mp3.type = 'video/mp3';
-
-		const t_vtt = document.createElement('track');
-		t_vtt.label = 'English';
-		t_vtt.kind = 'subtitles';
-		t_vtt.scrlang = 'en';
-		t_vtt.src = audioFiles[id]['subtitles'];
-		t_vtt.default = 'default';
-
-		let video = document.createElement('video');
-		video.appendChild(s_ogg);
-		video.appendChild(s_mp3);
-		video.appendChild(t_vtt);
-
-		video.id = 'video';
-
-		video.controls = false;
-		video.loop = false;
-
-		video.style.display = 'none';
-
-		let metadataTrack = video.textTracks[0];
-
-		video.addEventListener('loadedmetadata', () => {
+		if (!progress.getAttribute('max')) {
 			progress.setAttribute('max', video.duration);
-		});
-
-		video.addEventListener('timeupdate', () => {
-			if (!progress.getAttribute('max')) progress.setAttribute('max', video.duration);
 			progress.value = video.currentTime;
-			progressBar.style.width = `${Math.floor((video.currentTime * 100) / video.duration)}%`;
-		});
-
-		metadataTrack.addEventListener('cuechange', (event) => {
-			// console.log(metadataTrack.activeCues);
-			if (metadataTrack.activeCues.length > 0) {
-				console.log(metadataTrack.activeCues[0]);
-				containerScript.innerHTML += metadataTrack.activeCues[0].text + ' ';
-			}
-		});
-		6;
-
-		const container = document.getElementById('containerControls');
-
-		container.append(video);
-		containerScript.innerHTML = '';
-
-		const newVideoObj = document.getElementById('video');
-		newVideoObj.load();
-		newVideoObj.play();
-		isPlaying = true;
-
-		audioFiles[id]['isActive'] = true;
+		}
+		progress.value = video.currentTime;
 	}
 
-	function loadAudio(id) {
-		const video = document.getElementById('video');
-		video.load();
+	function addTextToScript(event, index, metadataTrack) {
+		if (metadataTrack.activeCues.length > 0) {
+			const containerScript = document.getElementById('containerScript' + index);
+			containerScript.innerHTML += metadataTrack.activeCues[0].text + ' ';
+		}
 	}
+
+	onMount(() => {
+		const videoElements = document.getElementsByTagName('video');
+
+		for (let i = 0; i < videoElements.length; i++) {
+			const video = videoElements[i];
+
+			let metadataTrack = video.textTracks[0];
+
+			video.loop = false;
+			video.controls = false;
+
+			video.addEventListener('loadedmetadata', (event) => setProgressDuration(event, i, video));
+
+			video.addEventListener('timeupdate', (event) => updateProgress(event, i, video));
+
+			metadataTrack.addEventListener('cuechange', (event) =>
+				addTextToScript(event, i, metadataTrack)
+			);
+		}
+		// for
+	});
 </script>
 
 <div class="containerControls" id="containerControls">
-	<!-- svelte-ignore a11y-media-has-caption -->
-	<video id="video" controls preload="metadata">
-		<source src="../audio/001_background-history.ogg" type="video/ogg" />
-		<source src="../audio/001_background-history.mp3" type="video/mp3" />
-		<track
-			label="English"
-			kind="subtitles"
-			srclang="en"
-			src="../audio/001_background-history.vtt"
-			default
-		/>
-	</video>
-
-	<div id="videoControls" class="controls" data-state="hidden">
-		<div class="progress">
-			<progress id="progress" value="0" min="0">
-				<span id="progress-bar"></span>
-			</progress>
-		</div>
-	</div>
-	<!-- videoControls -->
-
 	{#each audioFiles as track, index}
 		<div id="containerAudio{index}" class="containerAudio">
 			<h6>{track.label}</h6>
 
-			{#if audioFiles[index]['isActive'] === true}
-				<button
-					id="buttonLoadAudio{index}"
-					on:click={() => {
-						pauseAudio();
-					}}
-				>
-					{#if isPlaying === true}
-						Pause
-					{:else}
-						Play
-					{/if}
-				</button>
-			{:else}
-				<button
-					id="buttonLoadAudio{index}"
-					on:click={() => {
-						loadAudioFile(index);
-					}}>Load Track {index}</button
-				>
-			{/if}
+			<button
+				class="play"
+				id="buttonLoadAudio{index}"
+				on:click={() => {
+					pauseAudio(index);
+				}}
+			>
+				{#if isPlaying === index}
+					<i class="fa-solid fa-pause"></i>
+				{:else}
+					<i class="fa-solid fa-play"></i>
+				{/if}
+			</button>
+
+			<progress id="progress{index}" value="0" min="0"> </progress>
 
 			<div id="containerScript{index}"></div>
 			<!-- containerScript -->
+
+			<!-- svelte-ignore a11y-media-has-caption -->
+			<video
+				id="video{index}"
+				data-index={index}
+				controls="false"
+				preload="metadata"
+				style="display:none"
+			>
+				<source src={track.ogg} type="video/ogg" />
+				<source src={track.mp3} type="video/mp3" />
+				<track label="English" kind="subtitles" srclang="en" src={track.subtitles} default />
+			</video>
 		</div>
 		<!-- containerAudio -->
 	{/each}
@@ -240,7 +187,7 @@
 	#containerControls {
 		height: 90vh;
 		left: 5vh;
-		overflow: scroll;
+		overflow: auto;
 		position: absolute;
 		top: 5vw;
 		width: 24rem;
@@ -257,5 +204,18 @@
 		border: 1px solid black;
 
 		padding: 1rem;
+	}
+
+	button.play {
+		border: 0;
+		background-color: var(--accentColor);
+		border-radius: var(--toggleBorderRadius);
+		height: 2rem;
+		text-align: center;
+		width: 2rem;
+	}
+
+	button.play i {
+		padding: 0.325rem 0 0 0.025rem;
 	}
 </style>
