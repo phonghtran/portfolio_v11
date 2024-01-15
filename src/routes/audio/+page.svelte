ebfcc36709ecfee98629124948d7d03185a7c440
phonghtran <phong@phonghtran.com>
2024-01-14 21:35:49 -0500

    working playlist for audio. single video object

--- src/routes/audio/+page.svelte
+++ src/routes/audio/+page.svelte
@@ -3,16 +3,61 @@
 
 	import { onMount } from 'svelte';
 
-	const audioFiles = [
+	let isPlaying = false;
+
+	let audioFiles = [
 		{
 			id: 0,
+			label: 'History and background ',
+			ogg: '../audio/001_background-history.ogg',
+			mp3: '../audio/001_background-history.mp3',
+			subtitles: '../audio/001_background-history.vtt',
+			isActive: false
+		},
+		{
+			id: 1,
+			label: 'What’s your design process? ',
+			ogg: '../audio/002_design-process.ogg',
+			mp3: '../audio/002_design-process.mp3',
+			subtitles: '../audio/002_design-process.vtt'
+		},
+		{
+			id: 2,
+			label: 'What are your strengths? ',
+			ogg: '../audio/003_strengths.ogg',
+			mp3: '../audio/003_strengths.mp3',
+			subtitles: '../audio/003_strengths.vtt'
+		},
+		{
+			id: 3,
+			label: 'What are your weaknesses? ',
+			ogg: '../audio/004_weaknesses.ogg',
+			mp3: '../audio/004_weaknesses.mp3',
+			subtitles: '../audio/004_weaknesses.vtt'
+		},
+		{
+			id: 4,
+			label: 'Tell me about a recent project you’re proud of? ',
+			ogg: '../audio/005_recent-project-ai.ogg',
+			mp3: '../audio/005_recent-project-ai.mp3',
+			subtitles: '../audio/005_recent-project-ai.vtt'
+		},
+		{
+			id: 5,
+			label: 'Tell me about a time where you had a hurdle',
+			ogg: '../audio/006_hurdle-ai.ogg',
+			mp3: '../audio/006_hurdle-ai.mp3',
+			subtitles: '../audio/006_hurdle-ai.vtt'
+		},
+		{
+			id: 6,
 			label: 'How do you determine success? ',
 			ogg: '../audio/007_success.ogg',
 			mp3: '../audio/007_success.mp3',
 			subtitles: '../audio/007_success.vtt'
 		},
 		{
-			id: 1,
+			id: 7,
 			label: 'What is your work and leadership style?',
 			ogg: '../audio/008_leadership-style.ogg',
 			mp3: '../audio/008_leadership-style.mp3',
@@ -20,10 +65,21 @@
 		}
 	];
 
+	/** @param {MouseEvent} event */
+	function pauseAudio(event) {
+		console.log('i see');
+		const video = document.getElementById('video');
+		if (video.paused || video.ended) {
+			video.play();
+			isPlaying = true;
+		} else {
+			video.pause();
+			isPlaying = false;
+		}
+	}
 	function resetVideo() {
 		let video;
 		let metadataTrack;
-		console.log('reset');
 
 		var elems = document.getElementsByTagName('video');
 		for (let i = 0; i < elems.length; i++) {
@@ -40,15 +96,19 @@
 			thisVideo.remove();
 		}
 
-		const progress = document.getElementById('progress');
+		for (let i = 0; i < audioFiles.length; i++) {
+			audioFiles[i]['isActive'] = false;
+		}
 
+		const progress = document.getElementById('progress');
 		progress.value = 0;
 	}
 
 	function loadAudioFile(id) {
+		console.log(id);
 		resetVideo();
 
-		const containerScript = document.getElementById('containerScript');
+		const containerScript = document.getElementById('containerScript' + id);
 		const progress = document.getElementById('progress');
 		const progressBar = document.getElementById('progress-bar');
 
@@ -67,18 +127,12 @@
 		t_vtt.src = audioFiles[id]['subtitles'];
 		t_vtt.default = 'default';
 
-		const t_captions = document.createElement('track');
-		t_captions.kind = 'captions';
-		t_captions.src = audioFiles[id]['subtitles'];
-
 		let video = document.createElement('video');
 		video.appendChild(s_ogg);
 		video.appendChild(s_mp3);
 		video.appendChild(t_vtt);
-		video.appendChild(t_captions);
-		video.id = 'video';
 
-		console.log('new children', video);
+		video.id = 'video';
 
 		video.controls = false;
 		video.loop = false;
@@ -100,82 +154,46 @@
 		metadataTrack.addEventListener('cuechange', (event) => {
 			// console.log(metadataTrack.activeCues);
 			if (metadataTrack.activeCues.length > 0) {
+				console.log(metadataTrack.activeCues[0]);
 				containerScript.innerHTML += metadataTrack.activeCues[0].text + ' ';
 			}
 		});
+		6;
 
-		const container = document.getElementById('container');
+		const container = document.getElementById('containerControls');
 
 		container.append(video);
-		containerScript.innerHTML = audioFiles[id]['label'];
+		containerScript.innerHTML = '';
 
 		const newVideoObj = document.getElementById('video');
 		newVideoObj.load();
 		newVideoObj.play();
+		isPlaying = true;
 
-		console.log('finishing');
+		audioFiles[id]['isActive'] = true;
 	}
 
 	function loadAudio(id) {
 		const video = document.getElementById('video');
 		video.load();
 	}
-
-	onMount(() => {
-		const supportsVideo = !!document.createElement('video').canPlayType;
-		if (supportsVideo) {
-			const playpause = document.getElementById('playpause');
-			const progress = document.getElementById('progress');
-
-			playpause.addEventListener('click', (e) => {
-				const video = document.getElementById('video');
-				if (video.paused || video.ended) {
-					video.play();
-				} else {
-					video.pause();
-				}
-			});
-
-			progress.addEventListener('click', (e) => {
-				const rect = progress.getBoundingClientRect();
-				const pos = (e.pageX - rect.left) / progress.offsetWidth;
-				video.currentTime = pos * video.duration;
-			});
-		} //supportsVideo
-	}); // mount
 </script>
 
-<div class="container" id="container">
+<div class="containerControls" id="containerControls">
+	<!-- svelte-ignore a11y-media-has-caption -->
 	<video id="video" controls preload="metadata">
-		<source src="../audio/008_leadership-style.ogg" type="video/ogg" />
-		<source src="../audio/008_leadership-style.mp3" type="video/mp3" />
+		<source src="../audio/001_background-history.ogg" type="video/ogg" />
+		<source src="../audio/001_background-history.mp3" type="video/mp3" />
 		<track
 			label="English"
 			kind="subtitles"
 			srclang="en"
-			src="../audio/008_leadership-style.vtt"
+			src="../audio/001_background-history.vtt"
 			default
 		/>
-		<track kind="captions" src="../audio/008_leadership-style.vtt" />
-		<!-- fallback for browsers that don't support video tag -->
-		<a href="../audio/008_leadership-style.mp3">download video</a>
 	</video>
 
-	audioFiles
-
-	<button
-		on:click={() => {
-			loadAudioFile(0);
-		}}>Load Track 7</button
-	>
-	<button
-		on:click={() => {
-			loadAudioFile(1);
-		}}>Load Track 8</button
-	>
 	<div id="videoControls" class="controls" data-state="hidden">
-		<button id="playpause" type="button" data-state="play">Play/Pause</button>
-
 		<div class="progress">
 			<progress id="progress" value="0" min="0">
 				<span id="progress-bar"></span>
@@ -184,13 +202,60 @@
 	</div>
 	<!-- videoControls -->
 
-	<div id="containerScript"></div>
-	<!-- containerScript -->
+	{#each audioFiles as track, index}
+		<div id="containerAudio{index}" class="containerAudio">
+			<h6>{track.label}</h6>
+
+			{#if audioFiles[index]['isActive'] === true}
+				<button
+					id="buttonLoadAudio{index}"
+					on:click={() => {
+						pauseAudio();
+					}}
+				>
+					{#if isPlaying === true}
+						Pause
+					{:else}
+						Play
+					{/if}
+				</button>
+			{:else}
+				<button
+					id="buttonLoadAudio{index}"
+					on:click={() => {
+						loadAudioFile(index);
+					}}>Load Track {index}</button
+				>
+			{/if}
+
+			<div id="containerScript{index}"></div>
+			<!-- containerScript -->
+		</div>
+		<!-- containerAudio -->
+	{/each}
 </div>
 
 <!-- container -->
 <style>
+	#containerControls {
+		height: 90vh;
+		left: 5vh;
+		overflow: scroll;
+		position: absolute;
+		top: 5vw;
+		width: 24rem;
+	}
+
+	h6 {
+		margin: 0;
+	}
 	#video {
 		display: none;
+	}
+
+	.containerAudio {
+		border: 1px solid black;
+
+		padding: 1rem;
 	}
 </style>
